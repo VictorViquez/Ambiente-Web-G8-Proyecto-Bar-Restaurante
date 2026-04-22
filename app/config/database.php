ee71e100f619686ec9c0608d7215969514a5b44a
Victor Viquez Hernandez <96802846+VictorViquez@users.noreply.github.com>
2026-04-22 16:06:28 -0600

    DB

--- app/config/database.php
+++ app/config/database.php
@@ -15,9 +15,8 @@ class Database {
         $this->conn = null;
 
         try{
-
             $this->conn = new PDO(
-                "mysql:host=".$this->host.";port=".$this->port.";dbname=".$this->db_name,
+                "mysql:host=".$this->host.";port=".$this->port.";dbname=".$this->db_name.";charset=utf8mb4",
                 $this->username,
                 $this->password
             );
@@ -25,7 +24,7 @@ class Database {
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         }catch(PDOException $exception){
-            echo "Error de conexión: " . $exception->getMessage();
+            die("Error de conexión: " . $exception->getMessage());
         }
 
         return $this->conn;
